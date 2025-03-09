682fc5b36f9f348968ee1dcb696fa9099c3024b6
sgkochar <sgkochar@gmail.com>
2025-03-09 14:49:07 +0200

    edit BasicBreadcrumbs component

--- src/components/BasicBreadcrumbs/BasicBreadcrumbs.tsx
+++ src/components/BasicBreadcrumbs/BasicBreadcrumbs.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
-import Link from '@mui/material/Link';
+import { Link } from 'react-router-dom';
+
 
 type BasicBreadcrumbsProps = {
   activePage: string
@@ -16,7 +17,7 @@ function BasicBreadcrumbs({activePage}:BasicBreadcrumbsProps) {
   return (
     <div role="presentation" onClick={handleClick}>
       <Breadcrumbs aria-label="breadcrumb">
-        <Link underline="hover" color="text.primary" href="/">
+        <Link  to="/" style={{fontSize: "16px", fontWeight: 500, color: "black"}}>
           Home
         </Link>
         <Typography sx={{ color: 'inherit' }}>{activePage}</Typography>
